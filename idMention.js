bf7016f201be51faff2348e34a4e06edded820f6
Omicron <rsamal.b305@gmail.com>
2021-10-10 10:56:31 +0530

    Added prefix display on mention

--- idMention.js
+++ idMention.js
@@ -1,9 +1,10 @@
-module.exports = (str) => 
+module.exports.idMention = (str) => 
 {
     if (str.startsWith("<@") && str.endsWith(">"))
     {
         let mentionId=str.slice(2,-1)
         if (mentionId.startsWith("!"))
             mentionId=mentionId.slice(1)
+        return mentionId
     }
 }
